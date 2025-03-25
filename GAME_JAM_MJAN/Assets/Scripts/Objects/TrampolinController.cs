86ab323930b8790c5998ce7a56ca6c02bb73938a
Joaral <joan.ariza@enti.cat>
2025-03-25 09:33:14 +0100

    salto y trampolin acabados

--- GAME_JAM_MJAN/Assets/Scripts/Objects/TrampolinController.cs
+++ GAME_JAM_MJAN/Assets/Scripts/Objects/TrampolinController.cs
@@ -6,7 +6,7 @@ public class TrampolinController : MonoBehaviour
 {
     public float bounceForce = 1000;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
