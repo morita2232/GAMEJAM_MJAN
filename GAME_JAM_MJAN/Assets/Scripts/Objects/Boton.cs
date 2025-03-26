cc0546f5a01f0085299ce55289e905726fe9f5d9
morita2232 <sebastian.mora@enti.cat>
2025-03-26 09:43:02 +0100

    A

--- GAME_JAM_MJAN/Assets/Scripts/Objects/Boton.cs
+++ GAME_JAM_MJAN/Assets/Scripts/Objects/Boton.cs
@@ -7,11 +7,13 @@ public class Boton : MonoBehaviour
 
     public GameObject pared;
     public GameObject paredDos;
+    public SpriteRenderer sr;    
+    public Sprite newsr;    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,8 +26,9 @@ public class Boton : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Bola")
         {
-            Destroy(pared);
+            Destroy(pared);           
             Destroy(paredDos);
+            sr.sprite = newsr;
         }
     }
 
